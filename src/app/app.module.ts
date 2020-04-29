44dfbfcf7b09008cb74ad415d9ac9867545d2f0e
Lashan7 <lashan8.chamodya@gmail.com>
2020-04-29 17:35:19 +0530

    Finalized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { NbThemeModule, NbCardModule, NbButtonModule } from "@nebular/theme";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    NbThemeModule.forRoot(),
+    NbCardModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
